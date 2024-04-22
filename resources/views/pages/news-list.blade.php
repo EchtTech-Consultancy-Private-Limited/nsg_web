8223ff17f0da792b30230eef2c5107ed1c6f002a
Gireesh <gireesh.shukla@echttech.com>
2024-04-22 12:26:45 +0530

    fixed bugs

--- resources/views/pages/news-list.blade.php
+++ resources/views/pages/news-list.blade.php
@@ -23,8 +23,8 @@
                 <div class="common-card p-5 mt-4">
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="common-table table-responsive">
-                                <table id="nsg_datatable" class="table table-striped mb-0">
+                            <div class="table-responsive">
+                                <table id="nsg_datatable" class="table table-striped table-bordered mb-0">
                                     <thead>
                                       <tr>
                                         <th scope="col" class="text-center">S.No</th>
