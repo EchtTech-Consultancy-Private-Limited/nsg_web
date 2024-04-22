f45b98ce16031d45f4a1fcf3a8abf0875a6a9d03
Gireesh <gireesh.shukla@echttech.com>
2024-04-22 17:18:09 +0530

    Update tender page

--- resources/views/pages/current-tender-list.blade.php
+++ resources/views/pages/current-tender-list.blade.php
@@ -7,18 +7,23 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-              <div class="breadcrumb-wrap mb-4">
-                <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-                  <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{route('/')}}">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Tenders</li>
-                    <li class="breadcrumb-item active" aria-current="page">{{$title}}</li>
-                  </ol>
-                </nav>
-              </div>
-              <h2 class="heading-black">
-                Tenders List
-              </h2>
+                <div class="breadcrumb-wrap mb-4">
+                    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{route('/')}}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Tenders</li>
+                            <li class="breadcrumb-item active" aria-current="page">{{$title}}</li>
+                        </ol>
+                    </nav>
+                </div>               
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2 class="heading-black">
+                    Tenders List
+                    </h2>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn btn-red px-3 py-2">Archive Tenders</a>
+                    </div>
+                </div>
             </div>
             <div class="col-md-12">
                 <div class="common-card p-5 mt-4">
@@ -27,21 +32,21 @@
                             <div class="table-responsive">
                                 <table id="nsg_datatable" class="table table-striped table-bordered mb-0">
                                     <thead>
-                                      <tr>
-                                        <th scope="col" class="text-center">S.No</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Tender No.</th>
-                                        <th scope="col">Download</th>
-                                        <th scope="col">Date of Release</th>
-                                        <th scope="col">End Date</th>
-                                        <th scope="col">Est.Cost(Rs.)</th>
-                                        <th scope="col">Timings</th>
-                                        <th scope="col">Remarks</th>
-                                        <th scope="col">Amendments</th>
-                                      </tr>
+                                        <tr>
+                                            <th scope="col" class="text-center">S.No</th>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Tender No.</th>
+                                            <th scope="col">Download</th>
+                                            <th scope="col">Date of Release</th>
+                                            <th scope="col">End Date</th>
+                                            <th scope="col">Est.Cost(Rs.)</th>
+                                            <th scope="col">Timings</th>
+                                            <th scope="col">Remarks</th>
+                                            <th scope="col">Amendments</th>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    @if(isset($tender_managements) && count($tender_managements)>0)
+                                        @if(isset($tender_managements) && count($tender_managements)>0)
                                         @foreach($tender_managements as $key=>$tender_management)
                                         <tr>
                                             <td class="text-center">{{$key+1}}</td>
@@ -49,12 +54,18 @@
                                             <td>{!! $tender_management->description_en !!}</td>
                                             <td class="views-field views-field-field-amount-rs- download" data-label="
                                             Request Doc">
-                                            <a href="{{ asset('resources/uploads/PageContentPdf/'.$tender_management->public_url) }}" download="" tabindex="0" target="_blank">
-                                                Download
-                                            </a> <i class="fa fa-file-pdf-o text-danger ms-2"></i>  <span class="size">({{$tender_management->pdfimage_size}})</span>
+                                                <a href="{{ asset('resources/uploads/PageContentPdf/'.$tender_management->public_url) }}"
+                                                    download="" tabindex="0" target="_blank">
+                                                    Download
+                                                </a> <i class="fa fa-file-pdf-o text-danger ms-2"></i> <span
+                                                    class="size">({{$tender_management->pdfimage_size}})</span>
                                             </td>
-                                            <td class="text-center">{{\Carbon\Carbon::parse($tender_management->startDate)->format('d-m-Y')}}</td>
-                                            <td class="text-center">{{\Carbon\Carbon::parse($tender_management->endDate)->format('d-m-Y')}}</td>
+                                            <td class="text-center">
+                                                {{\Carbon\Carbon::parse($tender_management->startDate)->format('d-m-Y')}}
+                                            </td>
+                                            <td class="text-center">
+                                                {{\Carbon\Carbon::parse($tender_management->endDate)->format('d-m-Y')}}
+                                            </td>
                                             <td class="text-center">{{ $tender_management->tender_cost??'00' }}</td>
                                             @php
                                             $start_datetime = new DateTime($tender_management->startDate);
@@ -67,9 +78,9 @@
                                             <td class="text-center">{{ $tender_management->amendments??'-' }}</td>
                                         </tr>
                                         @endforeach
-                                    @endif
+                                        @endif
                                     </tbody>
-                                  </table>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -77,5 +88,5 @@
             </div>
         </div>
     </div>
-  </section>
+</section>
 @endsection
