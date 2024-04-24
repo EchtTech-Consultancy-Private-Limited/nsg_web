18e5539e605abd61a3ab7a60dae312d0f3791d99
brijesh-echttech <brijesh.sharma@echttech.com>
2024-04-24 11:14:17 +0530

    bug fix

--- resources/views/master-page.blade.php
+++ resources/views/master-page.blade.php
@@ -146,7 +146,7 @@
                   <!-- Form Builder Data Begin -->
                   @if(isset($pageData->formbuilderdata) && count($pageData->formbuilderdata)>0)
                      <div class="table-responsive">
-                        <table id="nsg_datatableForm" class="display common-table" style="width:100%">
+                        <table id="nsg_datatable" class="display common-table" style="width:100%">
                            <thead>
                                  <tr>
                                     @if(isset($pageData->formDataTableHead) && count($pageData->formDataTableHead)>0)
@@ -162,11 +162,11 @@
                            @if(isset($pageData->formbuilderdata) && count($pageData->formbuilderdata)>0)
                               @foreach($pageData->formbuilderdata as $formbuilderdatas)
                                        <tr>
-                                          @foreach(json_decode($formbuilderdatas->content) as $key=>$fdata)
-                                             @if($key !=null && $key !='')
-                                             <td>{{$fdata}}</td>
-                                             @endif
-                                          @endforeach
+                                       @foreach($formbuilderdatas as $key=>$value)
+                                          @if($key !=null && $key !='')
+                                          <td>{{$value}}</td>
+                                          @endif
+                                       @endforeach
                                        </tr>
                               @endforeach
                            @endif
